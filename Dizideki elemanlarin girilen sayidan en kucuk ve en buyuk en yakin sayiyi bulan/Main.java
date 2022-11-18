f1e6f7e3a15cdd86f72d1eab22d43999647c318d
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2022-11-18 22:52:52 +0300

    Update Main.java

--- Dizideki elemanlarin girilen sayidan en kucuk ve en buyuk en yakin sayiyi bulan/Main.java
+++ Dizideki elemanlarin girilen sayidan en kucuk ve en buyuk en yakin sayiyi bulan/Main.java
@@ -6,25 +6,27 @@ public class Main {
     public static void main(String[] args) {
 
         // Girilen sayıdan küçük ve büyük en yakın sayıyı bulan program
-
         Scanner input = new Scanner(System.in);
+        
+        // Kullanıcının dizinin uzunluğunu belirlemesini istedim 
         System.out.print("Enter the lenght of array :  ");
         int lenght = input.nextInt();
-        Random random = new Random();
+        
         int[] arr = new int[lenght];
 
+        // Kullanıdan dizinin elemanlarını belirlemesini istedim 
         for (int i = 0 ; i < arr.length;i++){
             System.out.print("Enter a element of array : ");
             arr[i] = input.nextInt();
         }
 
-        // Kullanıcıdan değer aldım
+        // Kullanıcıdan hangi sayıya göre en yakın küçük ve büyük alacağım değeri aldım
         System.out.print("\n\nEnter a value : ");
         int n = input.nextInt();
         int min = arr[0], max = arr[0];
         int temp;
 
-        // Diziyi sıraladım küçükten büyüğe sıradım
+        // Diziyi sıraladım küçükten büyüğe for döngüsü ile sıradım. Array.short metodunu bilerek kullanmadım :)
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
@@ -34,7 +36,8 @@ public class Main {
                 }
             }
         }
-        System.out.println("\n\nArray sıralanmış hali : ");
+        // Arrayi aşağıdaki for algoritması ile küçükten büyüğe sıraladım 
+        System.out.println("\n\nArray ordered from smallest to largest : ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
@@ -47,6 +50,7 @@ public class Main {
                 break;
             }
         }
+        // Girilen sayıdan küçük en büyük ve girilen sayıdan küçük en büyük sayıyı yazdırdım
         System.out.println("\n\nMin value : " + min);
         System.out.println("Max value : " + max);
 
